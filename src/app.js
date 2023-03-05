401eab456afdf7a658d1b54d40ce8bfcc13225db
Zsiga Gergely <zsiga.g@freestart.hu>
2023-03-05 18:04:18 +0100

    Kezdéshez előkészitve

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*
+* File: app.js
+* Author: Zsiga Gergely
+* Copyright: 2023, Zsiga Gergely
+* Group: Szoft I-2-E
+* Date: 2023-03-05
+* Github: https://github.com/zsi-ga/gepida.git
+* Licenc: GNU GPL
+*/
+
