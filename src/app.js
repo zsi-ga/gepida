ff24a341e7ed89deb9e178af367cee97c500a685
Zsiga Gergely <zsiga.g@freestart.hu>
2023-03-05 18:50:47 +0100

    Táblázat kész

--- src/app.js
+++ src/app.js
@@ -8,3 +8,42 @@
 * Licenc: GNU GPL
 */
 
+const tbody = document.querySelector("#tbody");
+
+
+var empList = [
+    { name: "cassis", size: 28, use: "offroad", price: 557900 },
+    { name: "Alboin 900", size: 28, use: "trekking", price: 519900 },
+    { name: "Asgard", size: 29, use: "technikás utak", price: 519900 },
+    { name: "Ruga", size: 29, use: "hegyi", price: 372900 },
+    { name: "Reptila", size: 28, use: "városi", price: 308900 },
+    { name: "Sirmium", size: 29, use: "hegyi", price: 264900 }    
+];
+
+
+function createTable(){
+
+    empList.forEach((emp) => {
+        
+        let tr = document.createElement("tr");
+        let tdName = document.createElement("td");
+        let tdSize = document.createElement("td");
+        let tdUse = document.createElement("td");
+        let tdPrice = document.createElement("td");
+
+        tdName.textContent = emp.name;
+        tdSize.textContent = emp.size;
+        tdUse.textContent = emp.use;
+        tdPrice.textContent = emp.price;
+
+
+        tbody.append(tr);
+        tr.append(tdName);
+        tr.append(tdSize);
+        tr.append(tdUse);
+        tr.append(tdPrice);
+
+    });    
+}; 
+
+createTable();
